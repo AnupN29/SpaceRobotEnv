486a40a8996e02e2951e6e7c9cd1f19713152deb
anunag29 <nagdeveanup29@gmail.com>
2024-03-30 11:18:36 +0530

    Updated sac cnn

--- RL_algorithms/Torch/SAC/SAC_Image/memory.py
+++ RL_algorithms/Torch/SAC/SAC_Image/memory.py
@@ -19,10 +19,10 @@ class ReplayBuffer:
         self.ptr, self.size, self.max_size = 0, 0, size
 
     def store(self, obs, act, rew, next_obs, done):
-        self.img_obs_buf[self.ptr] = obs[0]
-        self.depth_obs_buf[self.ptr] = obs[1]
-        self.img_obs2_buf[self.ptr] = next_obs[0]
-        self.depth_obs2_buf[self.ptr] = next_obs[1]
+        self.img_obs_buf[self.ptr] = obs[0].cpu()
+        self.depth_obs_buf[self.ptr] = obs[1].cpu()
+        self.img_obs2_buf[self.ptr] = next_obs[0].cpu()
+        self.depth_obs2_buf[self.ptr] = next_obs[1].cpu()
         self.act_buf[self.ptr] = act
         self.rew_buf[self.ptr] = rew
         self.done_buf[self.ptr] = done
